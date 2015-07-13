5a1a3076628ca28603857b90bed3d324ecc4f753
Keita Sugama <sugama@jamadam.com>
2015-07-13 10:48:17 +0900

    fixed sha256

--- Casks/trolcommander.rb
+++ Casks/trolcommander.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'trolcommander' do
   version '0.9.6'
-  sha256 '8668dc093524c61dcb16f34311327dcff3f51cde62e8b668b5932c8a59768c42'
-
-  url 'https://github.com/trol73/trol-commander-dist/raw/master/0.9.6/trolcommander-0_9_6.app.tar.gz'
+  sha256 '2378192a989c83e73ffad84a93b281ba2d0f8aeaac5763a454c3d66308423904'
+  
+  url 'https://github.com/trol73/trol-commander-dist/blob/master/0.9.6/trolcommander-0_9_6.app.tar.gz?raw=true'
   name 'trolCommander'
   homepage 'http://trolsoft.ru/en/soft/trolcommander'
   license :gpl
